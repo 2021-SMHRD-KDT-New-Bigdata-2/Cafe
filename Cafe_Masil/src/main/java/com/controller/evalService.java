15aa1ef63d6e9f759784b6231dbefe1ee302633b
smhrd <smhrd@DESKTOP-1LRNFSP>
2021-10-14 14:15:11 +0900

    1

---
+++ Cafe_Masil/src/main/java/com/controller/evalService.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/evalService")
+public class evalService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("euc-kr");
+		String eval = request.getParameter("evaluation");
+		
+		
+		
+	}
+
+}
